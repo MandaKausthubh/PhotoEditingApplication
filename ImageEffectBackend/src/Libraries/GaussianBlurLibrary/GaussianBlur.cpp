8496ffc6beabdbfd705c65ab7929ef1f3f8a96cf
Manda Kausthubh <112932070+MandaKausthubh@users.noreply.github.com>
2024-01-02 19:10:04 +0530

    Update GaussianBlur.cpp

--- ImageEffectBackend/src/Libraries/GaussianBlurLibrary/GaussianBlur.cpp
+++ ImageEffectBackend/src/Libraries/GaussianBlurLibrary/GaussianBlur.cpp
@@ -2,17 +2,25 @@
 #include <algorithm>
 #include <cstdlib>
 #include <vector>
+#include <math.h>
 
 // Function to generate single dimensional Gaussian Distribution
-std::vector<float> GenerateGausianMatrix(int size){
+std::vector<float> GenerateGausianMatrix(int size, float sigma){
     std::vector<float> Gaussian(size);
-    Gaussian[0] = 0.39894;
-    for(int i = 1; i < size; i++) {
-        Gaussian[i] = Gaussian[i-1];
-        for(int j = 0; j < 2*i - 1; i++) {
-            Gaussian[i] *= 0.3678;
-        }
+    float sum = 0;
+    // Gaussian[0] = 0.39894;
+    for(int i = 0; i < size; ++i) {
+        // Gaussian[i] = Gaussian[i-1];
+        // for(int j = 0; j < 2*i - 1; i++) {
+        //     Gaussian[i] *= 0.3678;
+        // }
+
+        int x = i - size/2;
+        Gaussian[i] = exp(-( x * x ) / (2* sigma * sigma));
+        sum += Gaussian[i];
     }
+    for (int i = 0; i < size; ++i) { Gaussian[i] /= sum; }
+    
     return Gaussian;
 }
 
@@ -20,37 +28,30 @@ void GaussianBlur(std::vector<std::vector<Pixel>> &image, float radius) {
     // We Implement Gaussian BLur Using Convolutions with the Gaussian Distribution !!
 
     std::vector<std::vector<Pixel>> Temp (image.size(), std::vector<Pixel>(image[0].size()));
-
     int rows = image.size(), cols = image[0].size();
+    std::vector<float> Gaussian = GenerateGausianMatrix(1 + 2 * ceil(2 * radius), sigma);
 
-    std::vector<float> Gaussian = GenerateGausianMatrix((int)(radius * 100) + 1);
-
-    int NeighbourhoodSize = (int)(radius * 10);
+    int NeighbourhoodSize = 1 + 2 * ceil(2 * radius)
+    std::vector<std::vector<Pixel>> ImageWithPadding(image.size() + 2 * (NeighbourhoodSize/2), std::vector<Pixel>(image[0].size() + 2 * (NeighbourhoodSize / 2)));
 
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             // Here we calculate temp[i][j];
-            
-            float r_value = 0, g_value = 0, b_value = 0;
+            Pixel tempPix;
+            tempPix.r = 0;
+            tempPix.g = 0;
+            tempPix.b = 0;
 
-            for(int x = -NeighbourhoodSize / 2; x < (NeighbourhoodSize/2) + 1; x++) {
-                for(int y = -NeighbourhoodSize/2; y < (NeighbourhoodSize/2) + 1; y++) {
-                    int pix_r = 0, pix_g = 0, pix_b = 0;
-                    if(x + i >= 0 && y + j >= 0 && x + i < rows && y + j < cols) {
-                        pix_r = image[x + i][y + j].r;
-                        pix_g = image[x + i][y + j].g;
-                        pix_b = image[x + i][y + j].b;
-                    }
-
-                    r_value += pix_r * Gaussian[abs(x)] * Gaussian[abs(y)];
-                    g_value += pix_g * Gaussian[abs(x)] * Gaussian[abs(y)];
-                    b_value += pix_b * Gaussian[abs(x)] * Gaussian[abs(y)];
+            for(int m = 0; m < NeigbourhoodSize; ++m) {
+                for(int n = 0; n < NeighbourhoodSize; ++n) {
+                    int x = i + m - NeighbourhoodSize/2;
+                    int m = j + n - NeighbourhoodSize/2;
+                    tempPixel.r += Gaussian[m] * Gaussian[n] * ImageWithPadding[x][y].r;
+                    tempPixel.g += Gaussian[m] * Gaussian[n] * ImageWithPadding[x][y].g;
+                    tempPixel.b += Gaussian[m] * Gaussian[n] * ImageWithPadding[x][y].b;
                 }
             }
-            r_value = std::min(255, (int)r_value);
-            g_value = std::min(255, (int)g_value);
-            b_value = std::min(255, (int)b_value);
-            Temp[i][j] = Pixel{(int)r_value, (int)g_value, (int)b_value};
+            Temp[i][j] = tempPix;
         }
     }
 
